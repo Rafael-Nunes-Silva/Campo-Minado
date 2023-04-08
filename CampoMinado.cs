2ff1d6eb543014b150af10cc8b6da402d3f03dac
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-08 14:50:30 -0300

    Movi o block try/catch para a função PegarInputs e terminei o loop de jogo

--- CampoMinado.cs
+++ CampoMinado.cs
@@ -12,33 +12,70 @@ class CampoMinado
 
         Console.Write("Insira a coluna: ");
         string coluna = Console.ReadLine().ToUpper();
+        if (int.TryParse(coluna, out int i))
+            Console.WriteLine("****************************\nCOLUNA PRECISA SER UMA LETRA\n****************************");
 
         Console.Write("Insira a linha: ");
-        uint linha = Convert.ToUInt32(Console.ReadLine());
+        uint linha = 0;
+        try
+        {
+            linha = Convert.ToUInt32(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("***************************\nLINHA PRECISA SER UM NÚMERO\n***************************");
+            return PegarInputs();
+        }
 
         return new KeyValuePair<string, uint>(coluna, linha);
     }
 
     static void Main(string[] args)
     {
-        Campo campo = new Campo(Dificuldade.NORMAL);
+        Console.Write("Em qual dificuldade deseja jogar?\n0 - Fácil\n1 - Normal\n2 - Difícil\n: ");
+        uint dificuldade = 1;
+        try
+        {
+            dificuldade = Convert.ToUInt32(Console.ReadLine());
+        }
+        catch
+        {
+            Main(args);
+            return;
+        }
+        if (dificuldade < 0 || dificuldade > 2)
+        {
+            Main(args);
+            return;
+        }
 
+        Campo campo = new Campo((Dificuldade)dificuldade);
         campo.Desenhar();
+
+        campo.Preencher(PegarInputs());
+
+        Status statusDoJogo = Status.JOGANDO;
         do
         {
-            try
-            {
-                campo.Jogar(PegarInputs());
-            }
-            catch
-            {
-                campo.Desenhar();
-                continue;
-            }
-
+            statusDoJogo = campo.Jogar(PegarInputs());
+            
             campo.Desenhar();
-        } while (true);
+        } while (statusDoJogo == Status.JOGANDO);
 
-        Console.Read();
+        if (statusDoJogo == Status.VENCEU)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Parabens! Você venceu!");
+        }
+        else if (statusDoJogo == Status.PERDEU)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Você perdeu :(");
+        }
+
+        Console.ResetColor();
+        Console.Write("!Quer tentar de novo?(Y/n): ");
+        if (Console.ReadLine().ToUpper() == "Y")
+            Main(args);
     }
 }
