c3ff8f3b151e77647b621a2f0ae5bc47bf15c47c
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-07 22:10:16 -0300

    Adicionei um dicionario para mapear letras de A a Z em numeros de 0 a 26 e um array de cores do console

--- Campo.cs
+++ Campo.cs
@@ -21,18 +21,18 @@ public class Campo
 {
     Celula[][] celulas;
 
-    public Campo(Dificuldade dificuldade)
+    public Campo(Dificuldade dificuldade = Dificuldade.NORMAL)
     {
         celulas = new Celula[26][];
         for (uint i = 0; i < 26; i++)
             celulas[i] = new Celula[26];
 
-        Random r = new Random();
+        Random r = new Random(DateTime.Now.Second);
         for (uint i = 0; i < 26; i++)
         {
             for (uint j = 0; j < 26; j++)
             {
-                if (r.Next() % 10 == 0)
+                if (r.Next() % 100 == 0)
                     celulas[i][j] = Celula.BOMBA;
             }
         }   
@@ -44,6 +44,48 @@ public class Campo
         }
     }
 
+    static readonly Dictionary<string, uint> mapaLetraIndex = new Dictionary<string, uint>
+    {
+        { "A", 0 },
+        { "B", 1 },
+        { "C", 2 },
+        { "D", 3 },
+        { "E", 4 },
+        { "F", 5 },
+        { "G", 6 },
+        { "H", 7 },
+        { "I", 8 },
+        { "J", 9 },
+        { "K", 10 },
+        { "L", 11 },
+        { "M", 12 },
+        { "N", 13 },
+        { "O", 14 },
+        { "P", 15 },
+        { "Q", 16 },
+        { "R", 17 },
+        { "S", 18 },
+        { "T", 19 },
+        { "U", 20 },
+        { "V", 21 },
+        { "W", 22 },
+        { "X", 23 },
+        { "Y", 24 },
+        { "Z", 25 }
+    };
+
+    static readonly ConsoleColor[] cores =
+    {
+        ConsoleColor.Green,
+        ConsoleColor.DarkGreen,
+        ConsoleColor.DarkYellow,
+        ConsoleColor.Blue,
+        ConsoleColor.DarkBlue,
+        ConsoleColor.Magenta,
+        ConsoleColor.DarkMagenta,
+        ConsoleColor.DarkRed
+    };
+
     public void Desenhar()
     {
         Console.Clear();
@@ -53,22 +95,23 @@ public class Campo
 
         for(uint i = 0; i < 26; i++)
         {
-            Console.Write($"{((i < 10) ? " " + (i+1).ToString() : (i+1).ToString())} ");
+            Console.ResetColor();
+            Console.Write($"{((i < 9) ? " " + (i+1).ToString() : (i+1).ToString())} ");
             for(uint j=0; j < 26; j++)
             {
+                Console.ForegroundColor = cores[j % 8];
                 if (celulas[i][j] == Celula.VAZIA)
-                    Console.Write(" ");
+                    Console.Write("  ");
                 else if (celulas[i][j] == Celula.BOMBA)
-                    Console.Write("@");
-                Console.Write(" ");
+                    Console.Write("@ ");
                 // Console.Write(celulas[i][j]);
             }
             Console.Write("\n");
         }
+    }
 
-        Console.Write("\n");
-        Console.WriteLine(" 1 @ @ @     @ @");
-        Console.WriteLine(" 2   @     @ @ @");
-        Console.WriteLine(" 3     @ @   @ @");
+    void Jogar(string coluna, uint linha)
+    {
+
     }
 }
