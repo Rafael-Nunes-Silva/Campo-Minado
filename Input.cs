74631414425c7d38d6c0a8fc2c5b700dc4ad6da9
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-12 16:20:05 -0300

    Adição de bool flag no struct GameInput e texto antes do input

--- Input.cs
+++ Input.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 static public class InputHandler
 {
-    static readonly Dictionary<char, uint> letterIndexMap = new Dictionary<char, uint>
+    static readonly Dictionary<char, int> letterIndexMap = new Dictionary<char, int>
     {
         { 'A', 0 },
         { 'B', 1 },
@@ -39,10 +36,10 @@ static public class InputHandler
 
     public struct GameInput
     {
-        public readonly uint column, line;
+        public readonly int column, line;
         public readonly bool flag;
 
-        public GameInput(uint column, uint line, bool flag)
+        public GameInput(int column, int line, bool flag)
         {
             this.column = column;
             this.line = line;
@@ -52,22 +49,25 @@ static public class InputHandler
 
     static public GameInput[] GetGameInput(Table table)
     {
+        Console.ResetColor();
+        Console.Write("Insira onde deseja jogar: ");
+
         string input = Console.ReadLine().ToUpper();
 
-        Regex playRegex = new Regex("^[A-Z][0-9]?[0-9]"),
-            flagRegex = new Regex("-f$");
+        Regex playRegex = new Regex("[A-Z][0-9]?[0-9]"),
+            flagRegex = new Regex("-F$");
         MatchCollection playMatches = playRegex.Matches(input);
         Match flagMatch = flagRegex.Match(input);
 
         List<GameInput> gameInputs = new List<GameInput>(playMatches.Count);
         for (int i = 0; i < playMatches.Count; i++)
         {
-            uint column = letterIndexMap[playMatches[i].ToString()[0]] - 1,
-                line = Convert.ToUInt32(playMatches[i].ToString().Substring(1)) - 1;
+            int column = letterIndexMap[playMatches[i].ToString()[0]],
+                line = Convert.ToInt32(playMatches[i].ToString().Substring(1))-1;
 
             if (column < 0 || column > table.numOfColumns-1)
                 continue;
-            if (line < 0 || line > table.numofLines-1)
+            if (line < 0 || line > table.numOfLines-1)
                 continue;
 
             gameInputs.Add(new GameInput(column, line, flagMatch.Success));
