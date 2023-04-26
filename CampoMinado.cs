a5164a5bb2f571692cf4197301299a8fe39e10e1
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-04-26 17:37:42 -0300

    iniciei a separação entre os modos singleplayer e multiplayer

--- CampoMinado.cs
+++ CampoMinado.cs
@@ -11,11 +11,9 @@ class CampoMinado
                             "                       | |                                             \n" +
                             "                       |_|                                             \n\n";
 
-    static void MainMenu()
+    static void PrintTitle()
     {
-        Console.Clear();
-
-        for (int i=0, j=0; i < title.Length; i++)
+        for (int i = 0, j = 0; i < title.Length; i++)
         {
             Console.ForegroundColor = Table.colors[j % Table.colors.Length];
             Console.Write(title[i]);
@@ -26,37 +24,31 @@ class CampoMinado
         Console.ResetColor();
     }
 
-    static void Main(string[] args)
+    static void MainMenu()
     {
-        MainMenu();
-
-        Console.WriteLine("Em qual dificuldade deseja jogar?");
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("0 - Fácil");
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("1 - Normal");
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("2 - Difícil");
-        Console.ResetColor();
-        Console.Write(": ");
-        
-        Table.Difficulty difficulty = Table.Difficulty.NORMAL;
-        try
+        int mode = 0;
+        Console.Write("Qual modo deseja jogar?\n0 - Um jogador\n1 - Multiplayer\n: ");
+        try { mode = int.Parse(Console.ReadLine()); }
+        catch (Exception e)
         {
-            difficulty = (Table.Difficulty)Convert.ToInt32(Console.ReadLine());
-        }
-        catch
-        {
-            Main(args);
-            return;
-        }
-        if ((int)difficulty < 0 || (int)difficulty > 2)
-        {
-            Main(args);
+            MainMenu();
             return;
         }
 
-        Table table = new Table(difficulty);
+        switch (mode)
+        {
+            case 0:
+                Singleplayer();
+                break;
+            case 1:
+                Multiplayer();
+                break;
+        }
+    }
+
+    static void Singleplayer()
+    {
+        Table table = new Table(InputHandler.GetDifficulty());
         table.Draw();
 
         Table.GameStatus gameStatus = GameLoop(table);
@@ -74,6 +66,116 @@ class CampoMinado
 
         Console.ResetColor();
         Console.WriteLine($"O jogo durou {table.elapsedTime} segundos e você usou {table.flags} bandeiras de {table.maxFlags}");
+    }
+
+    static void Multiplayer()
+    {
+        Console.Write("Insira o IP do servidor: ");
+        string ip = Console.ReadLine();
+
+        int port = 6778;
+        Console.Write("Insira a porta do servidor: ");
+        try { port = int.Parse(Console.ReadLine()); }
+        catch (Exception e) {
+            Console.WriteLine("Valor invalido");
+            Multiplayer();
+        }
+
+        Console.Write("Insira como quer ser chamado: ");
+        string name = Console.ReadLine();
+
+        while (!Connector.Connect(ip, port, name))
+        {
+            Console.Write("Deseja tentar novamente?(Y/n): ");
+            if (Console.ReadLine().ToUpper() != "Y")
+                return;
+        }
+
+        Console.WriteLine("Conexão bem sucedida");
+
+        while (Connector.IsConnected())
+        {
+            switch (Console.ReadLine().ToLower())
+            {
+                case "-disconnect":
+                    Connector.Disconnect();
+                    break;
+                case "-create_room":
+                    Console.Write("Insira o nome da sala: ");
+                    string roomName = Console.ReadLine();
+
+                    int maxPlayers = 2;
+                    Console.Write("Insira o número máximo de jogadores: ");
+                    try { port = int.Parse(Console.ReadLine()); }
+                    catch (Exception e) 
+                    { 
+                        Console.WriteLine("Valor invalido");
+                        break;
+                    }
+
+                    while(!Connector.CreateRoom(roomName, maxPlayers))
+                    {
+                        Console.Write("Deseja tentar novamente?(Y/n): ");
+                        if (Console.ReadLine().ToUpper() != "Y")
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        /* Create room
+        Console.Write("Insira o nome da sala: ");
+        string roomName = Console.ReadLine();
+
+        int maxPlayers = 1;
+        Console.WriteLine("Insira a quantidade máxima de jogadores: ");
+        try { maxPlayers = int.Parse(Console.ReadLine());}
+        catch (Exception e)
+        {
+            Console.WriteLine("Valor invalido");
+            Multiplayer();
+        }
+
+        connect:
+        if (!Connector.Connect(ip, port, roomName, maxPlayers))
+        {
+            Console.Write("Falha ao conectar com o servidor\nDeseja tentar novamente?(Y/n): ");
+            if (Console.ReadLine() == "Y")
+                goto connect;
+        }
+        */
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        PrintTitle();
+        MainMenu();
+
+        /*
+        Table table = new Table(InputHandler.GetDifficulty());
+        table.Draw();
+
+        Table.GameStatus gameStatus = GameLoop(table);
+
+        if (gameStatus == Table.GameStatus.WON)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Parabens! Você venceu!");
+        }
+        else if (gameStatus == Table.GameStatus.LOST)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Você perdeu :(");
+        }
+
+        Console.ResetColor();
+        Console.WriteLine($"O jogo durou {table.elapsedTime} segundos e você usou {table.flags} bandeiras de {table.maxFlags}");
+        Console.Write("Quer tentar de novo?(Y/n): ");
+        if (Console.ReadLine().ToUpper() == "Y")
+            Main(args);
+        */
+
         Console.Write("Quer tentar de novo?(Y/n): ");
         if (Console.ReadLine().ToUpper() == "Y")
             Main(args);
